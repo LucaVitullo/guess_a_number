00ccdafe8624ce009d856b34e80eaf90e73c8d7e
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2023-08-09 17:47:53 +0300

    while loop add and display print

--- guess-a-number.py
+++ guess-a-number.py
@@ -2,7 +2,15 @@ import random
 
 def guess(x):
     random_number = random.randint(1,x)
-    guess=0   
+    guess=0
+    while guess != random_number:
+        guess = int(input(f'guess a anumber between 1 and {x}: '))
+        if guess< random_number:
+            print('Sorry, guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry, guess again. Too high.')
+        
+    print(f'yeah, congrats. you have guessed the number {random_number}') 
 
 guess(10)
     
